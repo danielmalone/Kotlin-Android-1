a5d0830f1d64c7ea269132a9f4e339c9d4757472
Daniel Malone <danieljmalone@gmail.com>
2017-05-22 19:58:55 -0500

    convert java to kotlin

--- app/src/main/java/com/finepointmobile/kotlin1/MainActivity.kt
+++ app/src/main/java/com/finepointmobile/kotlin1/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.finepointmobile.kotlin1
 
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
+        val fab = findViewById(R.id.fab) as FloatingActionButton
         setSupportActionBar(toolbar)
         val tv = findViewById(R.id.tv) as TextView
         tv.text = "Welcome to Kotlin!"
-        tv.textSize = 24.0F
+        tv.textSize = 32.0F
+        val user = User("Daniel", "Malone", "daniel@finepointmobile.com")
+        tv.text = user.firstName
+
+        for (i in 0..9) {
+            Log.d("MainActivity", "testing " + i)
+        }
+
+        fab.setOnClickListener {
+            Log.d("MainActivity", "FAB pressed!")
+        }
     }
 }
---
+++ app/src/main/java/com/finepointmobile/kotlin1/User.kt
@@ -0,0 +1,7 @@
+package com.finepointmobile.kotlin1
+
+/**
+ * Created by danielmalone on 5/22/17.
+ */
+
+class User(var firstName: String?, var lastName: String?, var email: String?)
